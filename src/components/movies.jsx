539f1c5a1d014be7a620aea646ce7c96e197642b
Michael Okello <okellofrost@gmail.com>
2022-07-27 15:31:50 +0300

    deleting messages

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -4,6 +4,14 @@ class Movies extends Component {
     state = { 
         movies : getMovies()
     };
+    handleDelete =(movie) =>{
+        const movies = this.state.movies.filter(m => m._id !== movie._id );
+        // this.setState({movies : movies}); long method
+
+        this.setState({movies});//short method
+        console.log(movie);
+    };
+
     render() { 
         return <table className="table">
             <thead>
@@ -12,15 +20,17 @@ class Movies extends Component {
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th></th>
                 </tr>
                 </thead>
                 <tbody>
                     {this.state.movies.map(movie =>(
-                        <tr>
+                        <tr key={movie._id}>
                         <td>{movie.title}</td>
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td><button onClick={ () => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                     </tr> ))}
                     
                 </tbody>
