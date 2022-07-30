34ed29e263b3c1eb7296d8e43463160606936379
Michael Okello <okellofrost@gmail.com>
2022-07-30 17:35:41 +0300

    extracting the movies table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import { genres, getGenres } from '../services/fakeGenreService';
-import Like from './common/like';
 import Pagination from './common/pagination';
 import { paginate } from '../utils/paginate';
 import ListGroup from './common/listGroup';
+import MoviesTable from './moviesTable';
 
 class Movies extends Component {
     state = { 
@@ -58,45 +58,24 @@ class Movies extends Component {
          : allMovies;
         const movies = paginate(filtered , currentPage, pageSize);
         return (
-            <div className='row'>
-
-                <div className="col-3">
+        <div className='row'>
+       
+        <div className="col-3">
                     {/* Define the interfce of this component */}
-                    <ListGroup 
+        <ListGroup 
                      
-                     onItemSelect={this.handleGenreSelect}
-                     selectedItem={this.state.selectedGenre}
-                     items={this.state.genres}
+        onItemSelect={this.handleGenreSelect}
+        selectedItem={this.state.selectedGenre}
+        items={this.state.genres}
                      
-                     ></ListGroup>
-                </div>
-                <div className="col">
-                <p>Showing {filtered.length} movies in the database</p>
-                <table className="table">
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Genre</th>
-                    <th>Stock</th>
-                    <th>Rate</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                    {movies.map(movie =>(
-                        <tr key={movie._id}>
-                        <td>{movie.title}</td>
-                        <td>{movie.genre.name}</td>
-                        <td>{movie.numberInStock}</td>
-                        <td>{movie.dailyRentalRate}</td>
-                        <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)}/></td>
-                        <td><button onClick={ () => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                    </tr> ))}
-                    
-                </tbody>
-           
-        </table>
+         ></ListGroup>
+        </div>
+        <div className="col">
+        <p>Showing {filtered.length} movies in the database</p>
+        <MoviesTable movies={movies}
+        onDelete={this.handleDelete}
+        onLike={this.handleLike}
+        ></MoviesTable>       
         {/* <Pagination itemsCount={this.state.movies.length}/> */}
         <Pagination 
          itemsCount={filtered.length} 
@@ -105,7 +84,7 @@ class Movies extends Component {
          onPageChange={this.handlePageChange}/>
        
 
-                </div>
+        </div>
                 
         </div>
         
