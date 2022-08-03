f91d9ab275d45f311605a1d0e5bd160ac0c46e47
Michael Okello <okellofrost@gmail.com>
2022-08-03 20:07:10 +0300

    Adding routes and redicrects to the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { Route, Switch ,Redirect } from 'react-router-dom';
 import {getMovies} from '../src/services/fakeMovieService';
 import Movies from './components/movies';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import Customers from './components/customers';
 function App() {
   return (
    <main className='container'>
-   
-    <Movies></Movies>
-   
+        <Switch>
+        <Route path="/movies" component={Movies} ></Route>
+        <Route  path="/customers" component={Customers}></Route>
+        <Route  path="/rentals" component={Rentals}></Route>
+        <Route  path="/not-found" component={NotFound}></Route>
+        <Redirect exact from="/" to="/movies"/>
+        <Redirect to="not-found" component={NotFound}/>
+        </Switch>
    </main>
   );
 }
---
+++ src/components/moviesForm.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const MoviesForm = () =>{
+
+return <h1>MoviesForm</h1>
+}
+export default MoviesForm;
