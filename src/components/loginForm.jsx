9446b310e821b128375ea721cbfd43c856494447
Michael Okello <okellofrost@gmail.com>
2022-08-04 21:33:15 +0300

    Adding events on submit button

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+    username = React.createRef();
+// password = React.createRef();
 
+    componentDidMount(){
+        this.username.current.focus();
+    }
+
+
+    handleSubmit = e => {
+        e.preventDefault();
+        //call the server, save changes & redirect user
+       // console.log('Submitted');
+      const username =  this.username.current.value;
+       // const password =  this.password.current.value;
+    };
     render() { 
         return (
             <div>
                 <h1>Login</h1>
-                <form >
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" className="form-control" />
+                    <input ref={this.username} id="username" type="text" className="form-control" />
                     </div>
                     <div className="form-group">
                     <label htmlFor="password">Password </label>
-                    <input id="password" type="text" className="form-control" />
+                    <input ref={this.password} id="password" type="text" className="form-control" />
                     </div>
                     <button className="btn btn-primary">Login</button>
                 </form>
