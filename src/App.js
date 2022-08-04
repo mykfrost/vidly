946a267e80930461d870ce65712213fbaa0ca536
Michael Okello <okellofrost@gmail.com>
2022-08-04 19:46:14 +0300

    Adding links to movies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import './App.css';
 import { Route, Switch ,Redirect } from 'react-router-dom';
+import React,{Component } from 'react';
 import {getMovies} from '../src/services/fakeMovieService';
 import Movies from './components/movies';
+import MovieForm from './components/movieForm';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import Customers from './components/customers';
+import NavBar from './components/navBar';
+import './App.css';
+import { Fragment } from 'react';
 function App() {
   return (
+  <React.Fragment>
+    <NavBar/>
    <main className='container'>
         <Switch>
+        <Route path="/movies/:id" component={MovieForm} />
         <Route path="/movies" component={Movies} ></Route>
         <Route  path="/customers" component={Customers}></Route>
         <Route  path="/rentals" component={Rentals}></Route>
         <Route  path="/not-found" component={NotFound}></Route>
         <Redirect exact from="/" to="/movies"/>
-        <Redirect to="not-found" component={NotFound}/>
+        <Redirect to="not-found" />
         </Switch>
    </main>
+   </React.Fragment>
   );
 }
 
--- src/components/moviesForm.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const MoviesForm = () =>{
-
-return <h1>MoviesForm</h1>
-}
-export default MoviesForm;
