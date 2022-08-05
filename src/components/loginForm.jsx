4f087408370d373c550953996a63b1aa658325de
Michael Okello <okellofrost@gmail.com>
2022-08-05 15:34:39 +0300

    Using Joi Methods

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -11,23 +11,26 @@ class LoginForm extends Component {
 //Define Joi Schema 
 
    schema = {
-    username : Joi.string().required(),
-    password : Joi.string().required()
+    username : Joi.string().required().label('Username'),
+    password : Joi.string().required().label('Password')
    };
 
-
-  
    validate = () =>{
    const result = Joi.validate(this.state.account , this.schema ,{abortEarly : false});
     console.log(result);
-
+    if(!result.error) return null;
     const errors = {};
-    const {account}  = this.state;
-    if(account.username.trim() === '')
-        errors.username = 'Username is Required!';
-         if(account.password.trim() === '')
-        errors.password = 'Password is Required!';
-        return Object.keys(errors).length === 0 ? null : errors;
+
+    for(let item of result.error.details)
+    errors[item.path[0]] = item.message;
+    return errors;
+    // const errors = {};
+    // const {account}  = this.state;
+    // if(account.username.trim() === '')
+    //     errors.username = 'Username is Required!';
+    //      if(account.password.trim() === '')
+    //     errors.password = 'Password is Required!';
+    //     return Object.keys(errors).length === 0 ? null : errors;
    };
      validateProperty = ({name, value}) =>{
     if(name === "username"){
