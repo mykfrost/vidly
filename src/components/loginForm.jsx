add92522004a3b48c0892de2ee5286fcd54dae7d
Michael Okello <okellofrost@gmail.com>
2022-08-05 15:35:23 +0300

    Using Joi Methods

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -16,13 +16,12 @@ class LoginForm extends Component {
    };
 
    validate = () =>{
-   const result = Joi.validate(this.state.account , this.schema ,{abortEarly : false});
-    console.log(result);
-    if(!result.error) return null;
+   const {error}= Joi.validate(this.state.account , this.schema ,{abortEarly : false});
+    
+    if(!error) return null;
     const errors = {};
 
-    for(let item of result.error.details)
-    errors[item.path[0]] = item.message;
+    for(let item of error.details) errors[item.path[0]] = item.message;
     return errors;
     // const errors = {};
     // const {account}  = this.state;
