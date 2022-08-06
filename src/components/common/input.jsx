e46e31948d54ea2fbf35b68860d49bedb6584143
Michael Okello <okellofrost@gmail.com>
2022-08-06 15:35:55 +0300

    Changing the input interface

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -3,15 +3,9 @@ const Input = ({name ,label,  error , ...rest}) => {
     return (  
      <div className="form-group">
         <label htmlFor={name}>{label}</label>
-            <input  
-               {...rest}
-                name={name} 
-                id={name}                                            
-                className="form-control" 
-                />
-
-                {error && <div className="alert alert-danger">{error}</div>}
-                </div>               
+            <input {...rest} name={name} id={name} className="form-control"  />
+             {error && <div className="alert alert-danger">{error}</div>}
+    </div>               
 
     );
 };
