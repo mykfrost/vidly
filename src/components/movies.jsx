a590e5a649ce59df823af9dd8c925ec410476517
Michael Okello <okellofrost@gmail.com>
2022-08-13 21:03:34 +0300

    adding api backend

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -20,9 +20,9 @@ class Movies extends Component {
         sortColumn: {path : 'title' , order: 'asc'}
     };
     //rendered when rendering is done
-    componentDidMount(){
-        await re
-        const genres = [{_id: '' , name : 'All Genres'} , ...getGenres()];
+    async componentDidMount(){
+        const {data} = await getGenres();
+        const genres = [{_id: '' , name : 'All Genres'} , ...data];
         this.setState({movies : getMovies() , genres });
     }
     handleDelete = movie =>{
