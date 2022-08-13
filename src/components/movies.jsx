f2292c39e54a1b28f2ba1903159b79a16f89ff9b
Michael Okello <okellofrost@gmail.com>
2022-08-13 21:33:35 +0300

    adding movies backend

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
+import { deleteMovie, getMovies } from '../services/movieService';
 import {  getGenres } from '../services/genreService';
 import  _  from 'lodash';
+import {toast} from 'react-toastify';
 import Pagination from './common/pagination';
 import { paginate } from '../utils/paginate';
 import ListGroup from './common/listGroup';
@@ -23,13 +24,25 @@ class Movies extends Component {
     async componentDidMount(){
         const {data} = await getGenres();
         const genres = [{_id: '' , name : 'All Genres'} , ...data];
-        this.setState({movies : getMovies() , genres });
+
+        const {data : movies} = await  getMovies();
+        this.setState({movies , genres });
     }
-    handleDelete = movie =>{
-        const movies = this.state.movies.filter(m => m._id !== movie._id );
+    handleDelete = async movie =>{
+        const originalMovies = this.state.movies;
+
+        const movies = originalMovies.filter(m => m._id !== movie._id );
         // this.setState({movies : movies}); long method
 
         this.setState({movies});//short method
+        try{
+        await deleteMovie(movie._id);
+        }catch(ex){
+            if(ex.response && ex.response.status === 404)
+            toast.error('This movie has already been deleted');
+
+            this.setState({movies : originalMovies});
+        }
         //console.log(movie);
     };
 
